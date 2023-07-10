b802e53128b8b5a6e400eb124215cda933459ebf
Taehoon <lth9029@gmail.com>
2023-07-10 17:11:30 +0900

    Complete Step2-Read

--- main.py
+++ main.py
@@ -3,17 +3,52 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from flask import Flask, render_template
-
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'hello Taehoon'
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
+]
 
-@app.route('/coder')
-def coder():
-    return 'I am AutoCoder.'
+def template(contents, content):
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a><h1>
+            <ol>
+                {contents}
+            </ol>
+            {content}
+        </body>
+    </html>
+    '''
+
+def getContents():
+    list_tags = ''
+    for topic in topics:
+        list_tags = list_tags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+
+    return list_tags
+
+@app.route('/')
+def index():
+
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
+@app.route('/read/<int:id>/')
+def coder(id):
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+
+    return template(getContents(), f'<h2>{title}</h2>{body}')
 
 # .py가 실행되는 경로 안에 'templates' 폴더 생성 후 그 안에 test.html 파일 넣어 두기
 @app.route('/autocoder')
@@ -25,7 +60,7 @@ def test2():
     return render_template("test2.html")
 
 def main():
-    app.run(host='127.0.0.1', debug=False, port=80)
+    app.run(host='127.0.0.1', debug=True, port=80)
 
 
 if __name__ == '__main__':
